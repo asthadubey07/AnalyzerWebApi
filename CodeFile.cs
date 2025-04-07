e73a1f990cd3effb8b268d4db6f9dd06ec1b175d
asthadubey07 <asthadubey323@gmail.com>
2025-04-07 12:18:14 +0530

    cheaking

---
+++ CodeFile.cs
@@ -0,0 +1,10 @@
+﻿public class TestClass
+{
+    public string? Name;
+
+    public void DoStuff()
+    {
+        string? risky = null;
+        Console.WriteLine(risky.Length); // Nullable warning!
+    }
+}
