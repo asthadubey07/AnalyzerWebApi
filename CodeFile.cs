41da59f962046504c9d15bce800ab1c1bfddec79
asthadubey07 <asthadubey323@gmail.com>
2025-04-07 12:53:08 +0530

    fixed version

--- CodeFile.cs
+++ CodeFile.cs
@@ -1,10 +1,32 @@
-﻿public class TestClass
-{
-    public string? Name;
+﻿// <copyright>
+// Copyright (c) 2025 Astha.
+// Licensed under the MIT License.
+// </copyright>
 
-    public void DoStuff()
+namespace AnalyzerWebApi
+{
+    public class TestClass
     {
-        string? risky = null;
-        Console.WriteLine(risky.Length); // Nullable warning!
+        private string? name;
+
+        public string? Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public void DoStuff()
+        {
+            string? risky = null;
+
+            if (risky != null)
+            {
+                Console.WriteLine(risky.Length);
+            }
+            else
+            {
+                Console.WriteLine("risky is null");
+            }
+        }
     }
 }
